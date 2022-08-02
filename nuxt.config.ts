3cfb840837294b519a6b55dea66f4bc4a342f8a6
Dirceu Rezende <dirceu.srezende@gmail.com>
2022-08-02 20:40:56 -0300

    chore: add pinia and change srcDir to src

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,8 @@ import { defineNuxtConfig } from 'nuxt';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  srcDir: 'src',
+  modules: ['@pinia/nuxt'],
   experimental: {
     reactivityTransform: true,
     viteNode: false,
